2fe09f3100ac121b6ae5d14bbbf801ca9dedd13f
UmerKhalid02 <114934108+UmerKhalid02@users.noreply.github.com>
2024-02-06 23:38:47 +0500

    Added Mobile CORS

--- LearningApp.Web/Startup.cs
+++ LearningApp.Web/Startup.cs
@@ -61,6 +61,7 @@ namespace LearningApp.Web
                 .WithOrigins(
                 "http://127.0.0.1:8000",
                 "http://127.0.0.1:8001",
+                "http://127.0.0.1:8081",
                 "http://127.0.0.1:5000",
                 "http://127.0.0.1:5173",
                 "http://localhost:5173",
