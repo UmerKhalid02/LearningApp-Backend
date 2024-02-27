e2a0fe8326df1d9dd3c5c54fc928f9ccf1b20716
UmerKhalid02 <114934108+UmerKhalid02@users.noreply.github.com>
2024-02-27 22:42:48 +0500

    added full name in user table

--- LearningApp.Data/Entities/UserEntity/User.cs
+++ LearningApp.Data/Entities/UserEntity/User.cs
@@ -12,6 +12,7 @@ namespace LearningApp.Data.Entities.UserEntity
         public Guid UserId { get; set; }
         [Required]
         public string? UserName { get; set; }
+        public string? FullName { get; set; }
         [Required]
         public string? Email { get; set; }
         [Required]
