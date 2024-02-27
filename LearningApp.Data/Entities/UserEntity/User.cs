888e944b6ceb2d756fdf37cb56025a394be01404
UmerKhalid02 <114934108+UmerKhalid02@users.noreply.github.com>
2024-02-27 21:27:57 +0500

    added multiplier in User table

--- LearningApp.Data/Entities/UserEntity/User.cs
+++ LearningApp.Data/Entities/UserEntity/User.cs
@@ -1,10 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LearningApp.Data.Entities.UserEntity
 {
     [Table("User")]
-    public class User: BaseEntity
+    public class User : BaseEntity
     {
         [Key]
         [Column("UserID")]
@@ -16,6 +17,8 @@ namespace LearningApp.Data.Entities.UserEntity
         [Required]
         public string? Password { get; set; }
         public int? XP { get; set; }
+        [DefaultValue(1)]
+        public int Multiplier { get; set; } = 1;
 
         [Required]
         public virtual UserRole? UserRole { get; set; }
