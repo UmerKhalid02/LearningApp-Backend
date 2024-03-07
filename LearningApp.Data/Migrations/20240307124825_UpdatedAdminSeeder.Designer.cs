fe901202ea54fd977cf080920d50ce2ffb4bce7e
UmerKhalid02 <114934108+UmerKhalid02@users.noreply.github.com>
2024-03-07 17:49:03 +0500

    updated admin seeder

--- LearningApp.Data/Migrations/20240307124825_UpdatedAdminSeeder.Designer.cs
+++ LearningApp.Data/Migrations/20240307124825_UpdatedAdminSeeder.Designer.cs
@@ -4,6 +4,7 @@ using LearningApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LearningApp.Data.Migrations
 {
     [DbContext(typeof(EFDataContext))]
-    partial class EFDataContextModelSnapshot : ModelSnapshot
+    [Migration("20240307124825_UpdatedAdminSeeder")]
+    partial class UpdatedAdminSeeder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- LearningApp.Data/Seeders/AdminSeeder.cs
+++ LearningApp.Data/Seeders/AdminSeeder.cs
@@ -16,7 +16,8 @@ namespace LearningApp.Data.Seeders
                     FullName = "admin",
                     UserName = "admin",
                     Password = "admin123",
-                    Email = "admin@admin.com"
+                    Email = "admin@admin.com",
+                    IsActive = true,
                 });
 
             modelBuilder.Entity<UserRole>()
@@ -26,6 +27,7 @@ namespace LearningApp.Data.Seeders
                         UserRoleId = Guid.Parse("65AE02D5-E00F-433A-8D75-71C20B883C5F"),
                         UserId = Guid.Parse("B29A769C-2F0C-4A2B-A72D-71CDD4C98502"),
                         RoleId = RolesKey.AdminRoleId,
+                        IsActive = true
                     });
         }
     }
