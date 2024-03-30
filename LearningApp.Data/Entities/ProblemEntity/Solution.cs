29c18cd8684402b5e085db4e342de1b35845b3f6
UmerKhalid02 <114934108+UmerKhalid02@users.noreply.github.com>
2024-03-30 16:09:25 +0500

    added solution table

---
+++ LearningApp.Data/Entities/ProblemEntity/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearningApp.Data.Entities.ProblemEntity
+{
+    [Table("Solution")]
+    public class Solution : BaseEntity
+    {
+        [Key]
+        public Guid SolutionId { get; set; }
+        
+        [Required]
+        [Column("ProblemID")]
+        [ForeignKey("ProblemId")]
+        public virtual Guid ProblemId { get; set; }
+
+        [Required]
+        public string? SolutionText { get; set; }
+    }
+}
