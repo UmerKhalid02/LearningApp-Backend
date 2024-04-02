0cfdc4dda98be460d69fe82b49c7ab1ac06ce1d8
UmerKhalid02 <114934108+UmerKhalid02@users.noreply.github.com>
2024-04-02 22:06:11 +0500

    added more cors origin

--- LearningApp.Web/Startup.cs
+++ LearningApp.Web/Startup.cs
@@ -70,6 +70,8 @@ namespace LearningApp.Web
                 "http://127.0.0.1:5000",
                 "http://127.0.0.1:5173",
                 "http://localhost:5173",
+                "http://127.0.0.1:5500",
+                "http://127.0.0.1",
                 "http://localhost")
                 .AllowAnyMethod()
                 .AllowAnyHeader()
