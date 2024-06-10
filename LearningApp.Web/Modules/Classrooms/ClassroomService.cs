8781f9871c3950755a638212ee887c5de0877a97
UmerKhalid02 <114934108+UmerKhalid02@users.noreply.github.com>
2024-06-10 21:49:14 +0500

    FIX: fixed add topic in classroom api error

--- LearningApp.Web/Modules/Classrooms/ClassroomService.cs
+++ LearningApp.Web/Modules/Classrooms/ClassroomService.cs
@@ -163,7 +163,7 @@ namespace LearningApp.Web.Modules.Classrooms
                 throw new KeyNotFoundException(GeneralMessages.InvalidTopicId);
             }
 
-            classroom.Topics.Add(topic);
+            //classroom.Topics.Add(topic);
             topic.ClassroomId = classroom.ClassroomId;
             topic.Classroom = classroom;
             await _topicRepository.SaveChanges();
